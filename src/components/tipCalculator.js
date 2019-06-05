5c3b8c163579bf4f8ee98dd07ff1725b9a0a0d24
MAwais <mawais54013@outlook.com>
2019-06-04 21:26:36 -0700

    added basic form

--- src/components/tipCalculator.js
+++ src/components/tipCalculator.js
@@ -1,9 +1,63 @@
 import React from 'react';
 
-const tipCalculator = () => {
-    return (
-        <h1>div 1 </h1>
-    )
+class TipCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '0.00',
+            tip: '15',
+            people: '1',
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleTipChange = this.handleTipChange.bind(this);
+        this.handlePeopleChange = this.handlePeopleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+
+      handleTipChange(event) {
+        this.setState({tip: event.target.value});
+      }
+
+      handlePeopleChange(event) {
+        this.setState({people: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        console.log(this.state.tip)
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Bill:
+              <br/>
+              <input type="number" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <br/>
+            <label>
+                Tip %:
+                <br />
+                <input type="text" value={this.state.tip} onChange={this.handleTipChange} />
+            </label>
+            <br />
+            <br/>
+            <label>
+                Number of People: 
+                <br />
+                <input type="number" value={this.state.people} onChange={this.handlePeopleChange} />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
 }
 
-export default tipCalculator;
+export default TipCalculator;

32900ea1f24cc1d93067e87e0096c52eca328fe9
MAwais <mawais54013@outlook.com>
2019-06-07 17:41:08 -0700

    code review

--- src/components/tipCalculator.js
+++ src/components/tipCalculator.js
@@ -9,94 +9,83 @@ class TipCalculator extends React.Component {
             tip: '15',
             people: '1',
         };
-    
+
         this.handleChange = this.handleChange.bind(this);
         this.handleTipChange = this.handleTipChange.bind(this);
         this.handlePeopleChange = this.handlePeopleChange.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+    }
 
-      handleTipChange(event) {
-        this.setState({tip: event.target.value});
-      }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
 
-      handlePeopleChange(event) {
-        this.setState({people: event.target.value});
-      }
-    
-      render() {
+    handleTipChange(event) {
+        this.setState({ tip: event.target.value });
+    }
 
-          let tipNum = (parseFloat(this.state.value) * (parseInt(this.state.tip)/100))/parseInt(this.state.people) + 0.00;
-          let tipTotal;
-          tipTotal = parseFloat(tipNum).toFixed(2);
+    handlePeopleChange(event) {
+        this.setState({ people: event.target.value });
+    }
 
-          let totalNum = (parseFloat(this.state.value) + (parseFloat(tipTotal) * parseInt(this.state.people)))/parseInt(this.state.people);
-          console.log(totalNum)
-          let fullTotal;
-          
-          if(isNaN(totalNum) || totalNum < 0)
-          {
-              tipTotal = 'All fields must be positive numbers';
-              fullTotal = 'All fields must be positive numbers';
-          }
-          else
-          {
-            if(this.state.people > 1)
-            {
+    render() {
+        let tipNum = (parseFloat(this.state.value) * (parseInt(this.state.tip) / 100)) / parseInt(this.state.people) + 0.00;
+        let tipTotal;
+        tipTotal = parseFloat(tipNum).toFixed(2);
+
+        let totalNum = (parseFloat(this.state.value) + (parseFloat(tipTotal) * parseInt(this.state.people))) / parseInt(this.state.people);
+        let fullTotal;
+
+        if (isNaN(totalNum) || totalNum < 0) {
+            tipTotal = 'All fields must be positive numbers';
+            fullTotal = 'All fields must be positive numbers';
+        }
+        else {
+            if (this.state.people > 1) {
                 fullTotal = '$' + parseFloat(totalNum).toFixed(2) + ' per person';
                 tipTotal = '$' + parseFloat(tipNum).toFixed(2) + ' per person'
-
             }
-            else 
-            {
+            else {
                 fullTotal = '$' + parseFloat(totalNum).toFixed(2);
                 tipTotal = '$' + parseFloat(tipNum).toFixed(2);
             }
-          }
+        }
 
         return (
-         <div id="div1">
-             <h2>Tip Calculator</h2>
-            <div className="div2">
-                <br/>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                    Bill Amount:
-                    <br/>
-                    <input type="number" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    <br/>
+            <div id="div1">
+                <h2>Tip Calculator</h2>
+                <div className="div2">
                     <br />
-                    <label>
-                        Tip %:
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Bill Amount:
+                    <br />
+                            <input type="number" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <br /> 
+                        <label>
+                            Tip %:
+                            <br/>
+                            <input type="text" value={this.state.tip} onChange={this.handleTipChange} />
+                        </label>
                         <br />
-                        <input type="text" value={this.state.tip} onChange={this.handleTipChange} />
-                    </label>
-                    <br/>
-                    <br />
-                    <br/>
-                    <label>
-                        Number of People: 
+                        <label>
+                            Number of People:
+                            <input type="number" value={this.state.people} onChange={this.handlePeopleChange} />
+                        </label>
                         <br />
-                        <input type="number" value={this.state.people} onChange={this.handlePeopleChange} />
-                    </label>
-                    <br />
-                </form>
-            </div>
+                    </form>
+                </div>
 
-            <div className='div3' id="divFix">
-                <h2>Tip</h2>
-                <p id="pTag">{tipTotal}</p>
+                <div className='div3' id="divFix">
+                    <h2>Tip</h2>
+                    <p id="pTag">{tipTotal}</p>
 
-                <h2>Total</h2>
-                <p id="pTag">{fullTotal}</p>
+                    <h2>Total</h2>
+                    <p id="pTag">{fullTotal}</p>
+                </div>
             </div>
-          </div>
         );
-      }
+    }
 }
 
 export default TipCalculator;
